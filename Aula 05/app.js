8d8e27d87de64aaa67108a02196b484250622f33
Claudio Sousa <cauhsousa@gmail.com>
2023-02-27 17:00:08 -0300

    mexendo com array e json

--- Aula 05/app.js
+++ Aula 05/app.js
@@ -12,6 +12,13 @@ const listaNomes = ['José', 'Maria', 'Luiz', 'Antônio', 'Ana', 'Carlos']
 
 const listaProdutos = ['celular', 'monitor', 'teclado', 'gabinete', 'hd', 'ssd']
 
+
+/* 
+* JSON é composto por chave (atributo) e valor
+* {nome : 'josé', celular : '9343858343', email : 'jose@gmail.com'}
+
+*/
+
 const percorrerArray = () => {
     
 
@@ -96,4 +103,171 @@ const removerProduto = (nomeProduto, lista) => {
 
 }
 
-removerProduto('celula', listaProdutos)
+const listagemProdutos = () => {
+
+    // let listProdutosJSON = 
+    // {
+    //     produtos : listaProdutos,
+    //     clientes : listaNomes
+    // }
+
+//     let listProdutosJSON = {};
+
+//     listProdutosJSON.produtos = listaProdutos;
+//     listProdutosJSON.clientes = listaNomes;
+
+
+//     console.log(listProdutosJSON);
+//     console.log(listProdutosJSON.produtos[1]);
+//     console.log(listProdutosJSON.clientes[5]);
+
+let listProdutosJSON = {};
+    let listProdutosArray = 
+        [
+            {
+            nome : 'Monitor', 
+            quantidade : 300 ,
+            marca : 'Dell',
+            valor : 1000,
+            descricao : 'Monitor DELL ...',
+            codigo : 1
+        },
+        {
+            nome : 'Monitor', 
+            quantidade : 200 ,
+            marca : 'Lg',
+            valor : 1300,
+            descricao : 'Monitor DELL ...',
+            codigo : 2
+        },
+        {
+            nome : 'Teclado', 
+            quantidade : 500 ,
+            marca : 'Multilaser',
+            valor : 600,
+            descricao : 'Teclado Multilaser',
+            codigo : 3
+    
+        },
+        {
+            nome : 'Teclado', 
+            quantidade : 500 ,
+            marca : 'Redragon',
+            valor : 350,
+            descricao : 'Teclado Redragon',
+            codigo : 4
+        },
+        {
+            nome : 'Teclado', 
+            quantidade : 500 ,
+            marca : 'Razer',
+            valor : 1350,
+            descricao : 'Teclado Razer',
+            codigo : 5
+        },
+        {
+            nome : 'Mouse', 
+            quantidade : 660 ,
+            marca : 'Logitech',
+            valor : 450,
+            descricao : 'Mouse Logitech',
+            codigo : 6
+        },
+        {
+            nome : 'Mouse', 
+            quantidade : 660 ,
+            marca : 'Hyperx',
+            valor : 550,
+            descricao : 'Mouse Hyperx',
+            codigo : 7
+        }
+    
+    ]
+
+    let listCoresDellArray =['preto', 'branco', 'cinza'];
+    let listCoresLgArray =['preto', 'cinza'];
+    let listCoresTecladoArray = ['preto', 'branco','rosa','cinza','azul'];
+    let listCoresMouseArray = ['preto', 'branco','rosa','cinza','azul','roxo','verde'];
+
+    // let contCoresDell = listCoresDellArray.length;
+    // let contCoresLG = listCoresLgArray.length;
+    // let contCoresTeclado = listCoresTecladoArray.length;
+    // let contCoresMouse = listCoresMouseArray.length;
+
+    //array para modelos
+    let listModelosMonitor = ['lcd', 'ips', 'oled', '4k', 'led'];
+    let listModelosTeclado = ['mecânico', 'semi-mecânicos', 'membrana', 'optico']
+
+
+    listProdutosJSON.produtos = listProdutosArray;  
+    
+    //adicionar cores ao monitor dell
+    listProdutosJSON.produtos[0].cores = listCoresDellArray;
+    listProdutosJSON.produtos[1].cores = listCoresLgArray;
+
+    //cores para teclado
+    listProdutosJSON.produtos[2].cores = listCoresTecladoArray;
+    listProdutosJSON.produtos[3].cores = listCoresTecladoArray;
+    listProdutosJSON.produtos[4].cores = listCoresTecladoArray;
+
+    //cores para mouse
+    listProdutosJSON.produtos[5].cores = listCoresMouseArray;
+    listProdutosJSON.produtos[6].cores = listCoresMouseArray;
+
+    //adicionar modelos aos monitores
+    listProdutosJSON.produtos[0].modelo = listModelosMonitor;
+    listProdutosJSON.produtos[1].modelo = listModelosMonitor;
+
+    //adicionar modelos para teclados
+    listProdutosJSON.produtos[2].modelo = listModelosTeclado;
+    listProdutosJSON.produtos[3].modelo = listModelosTeclado;
+    listProdutosJSON.produtos[4].modelo = listModelosTeclado;
+       
+    // console.log(listProdutosJSON)
+    // console.log('nome: ' + listProdutosJSON.produtos[1].nome)
+    // console.log('marca: ' + listProdutosJSON.produtos[1].marca)
+    // console.log('valor: ' + listProdutosJSON.produtos[1].valor)
+    // console.log('cores: ' + listProdutosJSON.produtos[1].cores[1])
+    // console.log('modelos: ' + listProdutosJSON.produtos[1].modelo[1])
+
+   
+    let contIndexProduto = 0
+    let contLengthArray = listProdutosArray.length;
+    
+
+    let resNome;
+    let resMarca;
+    let resValor;
+    let contCor = 0
+    let reset = contCor
+    while ( contIndexProduto < contLengthArray){
+
+        contCor = reset
+
+        resNome =  listProdutosJSON.produtos[contIndexProduto].nome;
+        resMarca =  listProdutosJSON.produtos[contIndexProduto].marca;
+        resValor =  listProdutosJSON.produtos[contIndexProduto].valor;
+
+        while( contCor < listProdutosJSON.produtos[contIndexProduto].cores[contCor].length ){
+
+            let resCor = [];
+            contCor += 1
+
+            console.log(resCor)
+        }
+
+       
+
+        
+
+        console.log(resNome, resMarca, resValor)
+
+        contIndexProduto += 1;
+
+        
+        
+    }
+  
+}
+
+listagemProdutos()
